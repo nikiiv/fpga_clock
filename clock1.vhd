e54eb7a20653dabc8aebb62a84599ce418d9e671
Nikolay Ivanchev <nikolay.ivanchev@gmail.com>
2021-02-14 11:59:02 +0200

    revert shared varables to singals

--- clock1.vhd
+++ clock1.vhd
@@ -3,6 +3,12 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity clock1 is 
+
+	generic (
+		CLOCK_MHZ 	: integer := 50;	
+		TICKS 		: integer := 2_500_000
+	);
+
 	port (
 		clk 	: in std_logic;
 		SEG 	: out std_logic_vector (6 downto 0) := (others => '1');
@@ -17,11 +23,10 @@ entity clock1 is
 		led4	: out std_logic := '1';
 		btn1  : in  std_logic := '1';
 		btn2  : in  std_logic := '1';
-		btn3  : in  std_logic := '1';
-		btn4  : in  std_logic := '1';
+--		btn3  : in  std_logic := '1';
+--		btn4  : in  std_logic := '1';
 		nRes	: in  std_logic := '1');
-	constant CLOCK_MHZ 	: integer := 50;	
-	constant TICKS 		: integer := 25_000_00;
+
 end entity;
 
 architecture rtl of clock1 is
@@ -73,11 +78,8 @@ architecture rtl of clock1 is
 	signal wt  : unsigned (15 downto 0) := (others => '0');
 	signal sec : std_logic := '0';
 
-	shared variable d1 : integer range 0 to 255 := 0;
-	shared variable d2 : integer range 0 to 255 := 0;
-	shared variable d3 : integer range 0 to 255 := 0;
-	shared variable d4 : integer range 0 to 255 := 0;
-	shared variable dot_on : std_logic := '1';
+	signal d1,d2,d3,d4 : integer range 0 to 9 := 0;
+	signal dot_on : std_logic := '1';
 	
 begin
 
@@ -96,7 +98,7 @@ begin
 		
 	
 	sec_counter: process(clk)
-		variable counter : integer := TICKS;
+		variable counter : integer range TICKS downto 0 := TICKS;
 	begin
 		if rising_edge(clk) then
 			counter := counter - 1;
@@ -109,17 +111,18 @@ begin
 	
 	
 	tick_clock: process(nRes, sec) 
-		variable seconds : integer range 0 to 255 := 0;
-		variable msSec   : integer range 0 to 255 := 0;
-		variable min : integer range 0 to 255 := 0;
-		variable hour : integer range 0 to 255 := 0;
+		variable seconds : integer range 0 to 60 := 0;
+		variable msSec   : integer range 0 to 10 := 0;
+		variable min : integer range 0 to 60 := 0;
+		variable hour : integer range 0 to 24 := 0;
 	begin
 		if (nRes = '0') then
-			d1 := 0;
-			d2 := 0;
-			d3 := 0;
-			d4 := 0;
+			d1 <= 0;
+			d2 <= 0;
+			d3 <= 0;
+			d4 <= 0;
 			seconds := 0;
+			msSec := 0;
 		elsif (rising_edge(sec)) then
 		
 			if x1 = '0' then	
@@ -140,7 +143,6 @@ begin
 			if (msSec = 10) then
 				seconds := seconds + 1;	
 				msSec := 0;
-				dot_on := not dot_on;
 			end if;
 
 			
@@ -148,7 +150,7 @@ begin
 		
 			if seconds = 60 then
 				seconds := 0;
-				dot_on := not dot_on;
+				dot_on <= not dot_on;
 				min := min + 1;
 				if min > 59 then
 					min := 0;
@@ -161,11 +163,11 @@ begin
 -- convert decimal h/min to two digits
 --		d4 := hour mod 10;
 
-		d2 := min / 10;
-		d1 := min - d2*10;
+		d2 <= min / 10;
+		d1 <= min - d2*10;
 		
-		d4 := hour / 10;
-		d3 := hour - d4*10;
+		d4 <= hour / 10;
+		d3 <= hour - d4*10;
 		
 --		d1 := min  rem 10;
 --		d2 := (min - d1) / 10;
